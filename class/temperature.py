82fdfb3fd98b8217f028ceeaddc4d4219782da2d
kennethborek <kennethborek@gmail.com>
2016-04-20 17:09:38 -0400

    created 2 files.

--- class/temperature.py
+++ class/temperature.py
@@ -1,1 +1,13 @@
-print("hi")
+def GetTemperatures():
+
+     Entry="yes"
+     while (Entry=="yes" or Entry=="Yes" ):
+         x = float(input("What is the temperature in Farenheit"))
+         c = (x - 32) / 1.8
+         print("The temperature in celsuis is",c)
+         Entry=input("Continue?yes or no")
+GetTemperatures()
+
+
+
+
