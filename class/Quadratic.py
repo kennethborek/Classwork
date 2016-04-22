41c8d86f46a583416280ddd6bb608ce2ee6a7183
kennethborek <kennethborek@gmail.com>
2016-04-22 14:49:13 -0400

    created 2 files.

---
+++ class/Quadratic.py
@@ -0,0 +1,27 @@
+#(A,B,C are variables)
+#
+#
+#
+# AX2 +BX + C
+
+def main():
+def QuadVariable():
+    print("Welcome to the program! Please enter the values for a,b,c,x at the respective prompts")
+    a=float(int(input("Value for a"))
+    b=float(int(input("Value for b"))
+    c=float(int(input("Value for c"))
+    x=float(int(input("Value for x"))
+    QuadEquation(a,b,c,x)
+def QuadEquation(a,b,c,x):
+
+    y= (a*(x*x))+ (b*x) + c
+
+
+        print("The value of A is",a)
+        print("The value of B is",b)
+        print("The value of c is",c)
+        print("The value of x is",x)
+        print("The following quadratic was entered: ax^2+bx=c")
+        print("The value of the quadratic is ",y)
+
+main()
