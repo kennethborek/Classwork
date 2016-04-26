c0bc87930f69484c675b19f455c1f077bab46471
kennethborek <kennethborek@gmail.com>
2016-04-26 10:51:20 -0400

    created 2 files.

--- class/sort minimum list.py
+++ class/sort minimum list.py
@@ -1,11 +1,11 @@
-#The following program result should be a sorted list focusing on the minimum value.
+# The following program result should be a sorted list focusing on the minimum value.
 def main():
     A = [5, 7, 15, 1, 8]
     print(A)
     i = 0
     min= A[0]
     print(i, " ", min)
-    while i > len(A) - 1:
+    while i < len(A) - 1:
         i = i + 1
         if A[i] < min:
             min = A[i]
